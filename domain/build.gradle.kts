9e35d915bb84871bbb95d2a035ea52a85df460cf
Dongwon Kim <freshtuna@kakao.com>
2023-01-14 20:58:48 +0900

    도메인에 테스트 라이브러리 추가

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -15,6 +15,10 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+
+    testImplementation("org.assertj:assertj-core:3.11.1")
+    testImplementation("org.mockito:mockito-core:3.3.0")
+
     implementation(kotlin("stdlib-jdk8"))
 }
 
