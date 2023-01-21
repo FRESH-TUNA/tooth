13632366da2525367bf92fa37c2b529d7fb889d8
Dongwon Kim <freshtuna@kakao.com>
2023-01-21 21:19:54 +0900

    로컬 멤버에 패스워드 규칙 검증 로직 추가

--- domain/src/main/kotlin/com/freshtuna/openshop/LocalMember.kt
+++ domain/src/main/kotlin/com/freshtuna/openshop/LocalMember.kt
@@ -1,12 +1,25 @@
 package com.freshtuna.openshop
 
+import java.util.regex.Pattern
+
 class LocalMember(
     id: String,
     name: String,
     nickname: String,
     roles: List<Role>,
     var localId: String,
-    var password: String
+    var password: String,
 ) : Member(id, name, nickname, roles) {
 
+    companion object {
+        private val PASSWORD_SATETY_REGEX = Pattern.compile(
+            "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#${'$'}!%^&+=])(?=\\S+${'$'}).{8,20}${'$'}")
+    }
+
+    /**
+     * 패스워드가 안전한지 검증한다.
+     */
+    fun checkPasswordRule(): Boolean {
+        return PASSWORD_SATETY_REGEX.matcher(this.password).matches()
+    }
 }
--- domain/src/test/kotlin/com/freshtuna/openshop/LocalMemberTest.kt
+++ domain/src/test/kotlin/com/freshtuna/openshop/LocalMemberTest.kt
@@ -54,4 +54,90 @@ class LocalMemberTest {
         assertEquals(member.localId, localId)
         assertEquals(member.password, password)
     }
+
+    @Test
+    @DisplayName("패스워드 규칙 검증")
+    fun checkPasswordRuleTest() {
+        /**
+         * given
+         * 사용자에 필요한 정보
+         */
+        // 고유 식별자
+        val id = "식별자"
+
+        // 개인정보 (실명)
+        val name = "김동원"
+
+        // 부가정보
+        val nickname = "신선한참치"
+
+        // 권한
+        val roles: List<Role> = Lists.emptyList()
+
+        /**
+         * given
+         * 로컬 사용자에 필요한 정보
+         */
+        // 로그인 ID
+        val localId = "freshtuna@kakao.com"
+
+        /**
+         * given
+         * 테스트에 필요한 규칙에 어긋나는 패스워드를 사용한 로컬사용자들을 모두 생성
+         */
+        val member1 = LocalMember(id, name, nickname, roles, localId, "1aB!1aB")
+        val member2 = LocalMember(id, name, nickname, roles, localId, "1aB!1aB!1aB!1aB!1aB!1")
+        val member3 = LocalMember(id, name, nickname, roles, localId, "aaB!aaB!")
+        val member4 = LocalMember(id, name, nickname, roles, localId, "1ab!1ab!")
+        val member5 = LocalMember(id, name, nickname, roles, localId, "1AB!1AB!")
+        val member6 = LocalMember(id, name, nickname, roles, localId, "1AB11AB1")
+        val member7 = LocalMember(id, name, nickname, roles, localId, "1AB!1AB ")
+
+        /**
+         * when and then
+         * 패스워드는 8자이상 20자 이하여야 한다.
+         */
+        assertEquals(member1.checkPasswordRule(), false)
+        assertEquals(member2.checkPasswordRule(), false)
+
+        /**
+         * when and then
+         * 패스워드는 적어도 하나의 숫자가 필요하다.
+         */
+        assertEquals(member3.checkPasswordRule(), false)
+
+        /**
+         * when and then
+         * 패스워드는 적어도 하나의 대문자가 필요하다.
+         */
+        assertEquals(member4.checkPasswordRule(), false)
+
+        /**
+         * when and then
+         * 패스워드는 적어도 하나의 소문자가 필요하다.
+         */
+        assertEquals(member5.checkPasswordRule(), false)
+
+        /**
+         * when and then
+         * 패스워드는 적어도 하나의 특수문자가 있어야 한다.
+         */
+        assertEquals(member6.checkPasswordRule(), false)
+
+        /**
+         * when and then
+         * 패스워드에는 공백문자가 포함되면 안된다.
+         */
+        assertEquals(member7.checkPasswordRule(), false)
+
+        /**
+         * given, when, then
+         * 테스트에 필요한 규칙이 준수된 패스워드를 사용한 로컬사용자들을 모두 생성
+         */
+        val normalMember1 = LocalMember(id, name, nickname, roles, localId, "1aB!1aB!1aB!1aB!1aB!")
+        val normalMember2 = LocalMember(id, name, nickname, roles, localId, "1aB!1aB!")
+
+        assertEquals(normalMember1.checkPasswordRule(), true)
+        assertEquals(normalMember2.checkPasswordRule(), true)
+    }
 }
