c4f19fe49156a83ffeb0d32e7b07bc82ff315b51
Dongwon Kim <freshtuna@kakao.com>
2023-01-30 21:14:30 +0900

    유저 업데이트 포트 생성

--- application/src/main/kotlin/com/freshtuna/openshop/member/out/MemberUpdatePort.kt
+++ application/src/main/kotlin/com/freshtuna/openshop/member/out/MemberUpdatePort.kt
@@ -1,4 +1,10 @@
 package com.freshtuna.openshop.member.out
 
-class MemberUpdatePort {
+import com.freshtuna.openshop.OAuthMember
+
+interface MemberUpdatePort {
+    /**
+     * 소셜 계정의 정보를 업데이트 한다.
+     */
+    fun updateMember(member: OAuthMember)
 }
