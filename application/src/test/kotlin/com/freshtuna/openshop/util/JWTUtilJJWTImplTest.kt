e22a059fc6d54265a882a30594a8fb690f41474f
Dongwon Kim <freshtuna@kakao.com>
2023-02-04 22:04:35 +0900

    (테스트) 구현체 생성 및 로컬유저 토큰 생성 테스트 추가

--- application/src/test/kotlin/com/freshtuna/openshop/util/JWTUtilJJWTImplTest.kt
+++ application/src/test/kotlin/com/freshtuna/openshop/util/JWTUtilJJWTImplTest.kt
@@ -36,4 +36,67 @@ class JWTUtilJJWTImplTest {
          */
         assertEquals(true, jwtUtil is JWTUtil)
     }
+
+    @Test
+    @DisplayName("로컬 멤버 액세스 토큰 생성 테스트")
+    fun generateAccessTokenForLocalMember() {
+        /**
+         * given
+         * 토큰을 생성하고 싶은 멤버, 토큰을 생성할 테스트 객체
+         */
+        val localMember = createLocalMember()
+        val jwtUtil = createJWTUtilJJWTImpl()
+
+        /**
+         * when
+         * 토큰 생성
+         */
+        val token = jwtUtil.generateAccessToken(localMember)
+
+        /**
+         * then
+         * 토큰은 문자열타입으로 반환되는지 체크
+         */
+        assertEquals(true, token is String)
+    }
+
+
+
+    private fun createJWTUtilJJWTImpl(): JWTUtil {
+        val key = "this is key this is key this is key this is key this is key this is key this is key"
+        val accessTokenExpiredMileSeconds = 5000L
+        val refreshTokenExpiredMileSeconds = 1000L
+
+        return JWTUtilJJWTImpl(key, accessTokenExpiredMileSeconds, refreshTokenExpiredMileSeconds)
+    }
+
+    private fun createLocalMember(): Member {
+        // 고유 식별자
+        val id = "식별자"
+
+        // 개인정보 (실명)
+        val name = "김동원"
+
+        // 부가정보
+        val nickname = "신선한참치"
+
+        // 권한
+        val roles: List<Role> = Lists.emptyList()
+
+        /**
+         * given
+         * 로컬 사용자에 필요한 정보
+         */
+        // 로그인 ID
+        val localId = "freshtuna@kakao.com"
+
+        // 패스워드
+        val password = "패스워드"
+
+        /**
+         * when
+         * 주어진 정보들을 통해 로컬 멤버 생성하기
+         */
+        return LocalMember(id, name, nickname, roles, localId, password)
+    }
 }
