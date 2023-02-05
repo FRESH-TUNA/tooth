10c4b91a09fa320667ef29f29878ea36cfa8ff6d
Dongwon Kim <freshtuna@kakao.com>
2023-02-05 21:43:15 +0900

    jwt 에러, 내부 서버 에러 추가

--- domain/src/main/kotlin/com/freshtuna/openshop/Error.kt
+++ domain/src/main/kotlin/com/freshtuna/openshop/Error.kt
@@ -5,5 +5,15 @@ enum class Error {
      * LocalMember
      */
     LOCAL_MEMBER_NOT_FOUNDED,
-    PASSWORD_NOT_MATCHED
+    PASSWORD_NOT_MATCHED,
+
+    /**
+     * JWT
+     */
+    JWT_ERROR,
+
+    /**
+     * internal server error
+     */
+    INTERNAL_SERVER_ERROR
 }
