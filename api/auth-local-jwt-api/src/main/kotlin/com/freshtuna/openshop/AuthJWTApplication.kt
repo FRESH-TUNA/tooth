977aab6f8413dcfe6a4f00fa35794696b8091390
Dongwon Kim <freshtuna@kakao.com>
2023-02-14 22:50:17 +0900

    로컬 jwt 인증 APP 생성

---
+++ api/auth-local-jwt-api/src/main/kotlin/com/freshtuna/openshop/AuthJWTApplication.kt
@@ -0,0 +1,10 @@
+package com.freshtuna.openshop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AuthJWTApplication
+fun main(args: Array<String>) {
+    runApplication<AuthJWTApplication>(*args)
+}
